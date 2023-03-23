bcf3263acb3bfe492bb8bc01a2b4b21d6cdddc51
Robert Steinberg <51616178+GoofyGoof1126@users.noreply.github.com>
2023-03-23 23:07:00 +0200

    Add files via upload

---
+++ CSharp-ATM-Console-App/ATMApp2/ATMApp/UI/AppScreen.cs
@@ -0,0 +1,135 @@
+﻿using ATMApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.UI
+{
+    public class AppScreen
+    {
+        internal const string cur = " R ";
+        internal static void Welcome()
+        {
+            //clears the console screen
+            Console.Clear();
+            //sets the title of the console window
+            Console.Title = "My ATM App";
+            //sets the text color or foreground color to white
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //set the welcome message 
+            Console.WriteLine("\n\n-----------------Welcome to My ATM App-----------------\n\n");
+            //prompt the user to insert atm card
+            Console.WriteLine("Please insert your ATM card");
+            Console.WriteLine("Note: Actual ATM machine will accept and validate" +
+                " a physical ATM card, read the card number and validate it.");
+            Utility.PressEnterToContinue();
+        }
+
+        internal static UserAccount  UserLoginForm()
+        {
+            UserAccount tempUserAccount = new UserAccount();
+
+            tempUserAccount.CardNumber = Validator.Convert<long>("your card number.");
+            tempUserAccount.CardPin = Convert.ToInt32(Utility.GetSecretInput("Enter your card PIN"));
+            return tempUserAccount;
+        }
+
+        internal static void LoginProgress()
+        {
+            Console.WriteLine("\nChecking card number and PIN...");
+            Utility.PrintDotAnimation();
+        } 
+
+        internal static void PrintLockScreen()
+        {
+            Console.Clear();
+            Utility.PrintMessage("Your account has been locked. Please go to the nearest branch" +
+                " to unlock your account. Thank you.", true);
+            Environment.Exit(1);
+        }
+
+        internal static void WelcomeCustomer(string fullName)
+        {
+            Console.WriteLine($"Welcome back, {fullName}");
+            Utility.PressEnterToContinue();
+        }
+
+        internal static void DisplayAppMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("-------My ATM App Menu-------");
+            Console.WriteLine(":                           :");
+            Console.WriteLine("1. Account Balance          :");
+            Console.WriteLine("2. Cash Deposit             :");
+            Console.WriteLine("3. Withdrawal               :");
+            Console.WriteLine("4. Transfer                 :");
+            Console.WriteLine("5. Transactions             :");
+            Console.WriteLine("6. Logout                   :");
+        }
+
+        internal static void LogoutProgress()
+        {
+            Console.WriteLine("Thank you for using My ATM App.");
+            Utility.PrintDotAnimation();
+            Console.Clear();
+        }
+
+        internal static int SelectAmount()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(":1.{0}500      5.{0}3500", cur);
+            Console.WriteLine(":2.{0}1000     6.{0}4000", cur);
+            Console.WriteLine(":3.{0}2500     7.{0}4500", cur);
+            Console.WriteLine(":4.{0}3000     8.{0}5000", cur);
+            Console.WriteLine(":0.Other");
+            Console.WriteLine("");
+
+            int selectedAmount = Validator.Convert<int>("option:");
+            switch (selectedAmount)
+            {
+                case 1:
+                    return 500;
+                    break;
+                case 2:
+                    return 1000;
+                    break;
+                case 3:
+                    return 2500;
+                    break;
+                case 4:
+                    return 3000;
+                    break;
+                case 5:
+                    return 3500;
+                    break;
+                case 6:
+                    return 4000;
+                    break;
+                case 7:
+                    return 4500;
+                    break;
+                case 8:
+                    return 5000;
+                    break;
+                case 0:
+                    return 0;
+                    break;
+                default:
+                    Utility.PrintMessage("Invalid input. Try again.", false);
+                    return -1;
+                    break;
+            }   
+        }
+        internal InternalTransfer InternalTransferForm()
+        {
+            var internalTransfer = new InternalTransfer();
+            internalTransfer.ReciepeintBankAccountNumber = Validator.Convert<long>("recipient's account number:");
+            internalTransfer.TransferAmount = Validator.Convert<decimal>($"amount {cur}");
+            internalTransfer.RecipientBankAccountName = Utility.GetUserInput("recipient's name:");
+            return internalTransfer;
+        }
+    }
+}
